98e70b930f396eff1e9accdb6642060e7e44e438
Joel sen <72008676+joelsen@users.noreply.github.com>
2022-02-22 21:29:51 +0530

    init commit

---
+++ deletefaculty.php
@@ -0,0 +1,13 @@
+
+<?php
+if (isset($_GET['recordId'])){
+    $recordId = $_GET['recordId'];
+    $mysqli= new mysqli('localhost', 'root', '','test') or die(mysqli_error($mysqli));
+
+   $delete= $mysqli->query("DELETE FROM faculty WHERE no='$recordId'") or die($mysqli->error);
+    if ($delete){
+        echo '<script>alert("Record deleted successfully !")</script>';
+        echo '<script>window.location.href="student_data.php";</script>';
+    }
+}
+?>
